c2c2ca18af5c93125041c8ec801ea5cc5c8b992c
gibroule <gibroule@pm.me>
2023-11-14 23:02:49 +0100

    Comm : début fonctions callback

--- platformIO/SerialTopicOnArduino_Test/src/main.cpp
+++ platformIO/SerialTopicOnArduino_Test/src/main.cpp
@@ -3,39 +3,65 @@
 
 #define SERIAL_BAUD 115200
 
-SerialTransfer transfer;
-char * s_message = (char*)"Coucou raspberry, je suis arduino.";
-char r_message[255];
-double y = 2.5;
+#define REQUEST_BYTE 'R'
+#define ANSWER_BYTE 'A'
 
-struct __attribute__((packed)) STRUCT {
-  char z;
-  double y;
-} testStruct;
+#define PING_OPCODE 0x00
+#define GETUUID_OPCODE 0x01
+#define SETUUID_OPCODE 0x02
+#define DISCONNECT_OPCODE 0x03
+#define GETEEPROM_OPCODE 0x04
+#define SETEEPROM_OPCODE  0x05
+#define GETBUFFERSIZE_OPCODE 0x06
+
+SerialTransfer transfer;
+uint16_t sendSize;
+
+/*
+* Fonctions de callback
+*/
+void callback_ping()
+{
+  sendSize = 0;
+  sendSize = transfer.txObj("pong.", sendSize);
+  transfer.sendData(sendSize);
+  //Serial.println("Test pong");
+}
+
+void callback_getuuid()
+{
+  sendSize = 0;
+  sendSize = transfer.txObj("1234", sendSize);
+  transfer.sendData(sendSize);
+}
+
+// Tableau de callbacks
+const functionPtr callbackArr[] = {callback_ping, callback_getuuid};
 
 
 void setup()
 {
-  // Init Serial
-  Serial.begin(SERIAL_BAUD);
-  transfer.begin(Serial);
-
-  testStruct.z ='$';
-  testStruct.y = 4.5;
+  configST myConfig;
+  myConfig.debug        = true;
+  myConfig.callbacks    = callbackArr;
+  myConfig.callbacksLen = sizeof(callbackArr) / sizeof(functionPtr);
   
+  Serial.begin(SERIAL_BAUD);
+  transfer.begin(Serial,myConfig);
 }
 
 void loop()
 {
+  /*
   //Envoi message
   uint16_t sendSize = 0;
   sendSize = transfer.txObj(testStruct, sendSize);
   transfer.sendData(sendSize);
   //Réception message
-  /*if(transfer.available()) {
+  if(transfer.available()) {
     transfer.rxObj(r_message);
     Serial.println(r_message);
-  }*/
-
-  delay(1000);
+  }
+  delay(1000);*/
+  transfer.tick();
 }
--- raspberrypi/common/serialtransfert.py
+++ raspberrypi/common/serialtransfert.py
@@ -3,25 +3,40 @@ from pySerialTransfer import pySerialTransfer as txfer
 
 #TODO: pensez à une trame efficace Faire avec des callback sur arduino uniquement?
 
+MASTER_BYTE = b'R'
+SLAVE_BYTE = b'A'
+
+PING_OPCODE = 0x00
+GETUUID_OPCODE = 0x01
+SETUUID_OPCODE = 0x02
+DISCONNECT_OPCODE = 0x03
+GETEEPROM_OPCODE = 0x04
+SETEEPROM_OPCODE = 0x05
+GETBUFFERSIZE_OPCODE = 0x06
+
+"""
 class struct(object):
     z = ''
     y = 0.0
+"""
+
 
 if __name__ == '__main__':
      #Seuleument pour test
     
     try:
-        testStruct = struct
-
-        link = txfer.SerialTransfer('COM3')
+        #testStruct = struct
+        link = txfer.SerialTransfer('/dev/ttyUSB0')
         
         link.open()
         time.sleep(2) # allow some time for the Arduino to completely reset
-        
         while True:
             send_size = 0
+            send_size = link.tx_obj(PING_OPCODE)
+            link.send(send_size)
 
             if link.available():
+                """
                 recSize = 0
 
                 testStruct.z = link.rx_obj(obj_type='c', start_pos=recSize)
@@ -31,7 +46,24 @@ if __name__ == '__main__':
                 testStruct.y = link.rx_obj(obj_type='f', start_pos=recSize)
                 recSize += txfer.STRUCT_FORMAT_LENGTHS['f']
     
-                print('{} {}'.format(testStruct.z, testStruct.y))
+                print('{} {}'.format(testStruct.z, testStruct.y))"""
+
+                recSize = 0
+                ping_data = link.rx_obj(obj_type=str)
+                recSize += len(ping_data)
+                print("Ping data : {}".format(ping_data))
+             
+            elif link.status < 0:
+                if link.status == txfer.CRC_ERROR:
+                    print('ERROR: CRC_ERROR')
+                elif link.status == txfer.PAYLOAD_ERROR:
+                    print('ERROR: PAYLOAD_ERROR')
+                elif link.status == txfer.STOP_BYTE_ERROR:
+                    print('ERROR: STOP_BYTE_ERROR')
+                else:
+                    print('ERROR: {}'.format(link.status))
+
+
     except KeyboardInterrupt:
         try:
             link.close()
